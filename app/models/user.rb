2b7b8b18801a3d5866e2d31e2ab5c7e5a6b5f47f
Cate Uselton <cathode.use@gmail.com>
2014-10-14 13:35:27 -0700

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :products
   has_many :orderitems, through: :products
+
+  validates :username, :email, presence: true, uniqueness: true
+  validates :email, format: {with: /@/}
 end

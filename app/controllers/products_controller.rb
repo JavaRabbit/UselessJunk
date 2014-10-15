cbbc18e3a36b5bb6abb59ee658b7918f48268a74
Kathy Lepe <kathylepe434@gmail.com>
2014-10-15 10:11:54 -0700

    fixed products controller conflicts

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,14 @@
 class ProductsController < ApplicationController
 
-<<<<<<< HEAD
+
   def index
     @products = Product.all
-=======
+  end
+
   def show
     @product = Product.find_by(id: params[:id])
     if @product == nil
       redirect_to "/"
     end
     @review = Review.new
->>>>>>> master
   end
-
-end

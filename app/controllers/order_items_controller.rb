d93579485ad08779fa9aa61d93ee96ba5d5e7796
Allie Sterling <sterlina89@gmail.com>
2014-10-16 16:29:15 -0700

    updating quantity to 0 destroys item from order

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -3,9 +3,15 @@ class OrderItemsController < ApplicationController
   def update
     params[:order_item].each do |k, v|
         order_item = OrderItem.find_by(id: k)
-        order_item.quantity_of_product = v
-        order_item.save
+        if v == "0"
+          order_item.destroy
+        else
+          order_item.quantity_of_product = v
+          order_item.save
+        end
+
     end
+
     redirect_to order_path("new_cart")
   end
 

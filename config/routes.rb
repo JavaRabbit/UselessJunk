5238ce4b9878189f9257c445a5fa69b1e389a460
Allie Sterling <sterlina89@gmail.com>
2014-10-20 10:15:51 -0700

    fixed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   delete "/products/:id",          to: "products#destroy",    as: :delete_product
 
   get "/order_items",             to: "order_items#index",    as: :order_items
-  #this page will show only the logged user's order_items
+  # this page will show only the logged user's order_items
   patch "/update_cart",           to: "order_items#update",  as: :update_cart
 
   get "/reviews",                 to: "reviews#index",      as: :reviews

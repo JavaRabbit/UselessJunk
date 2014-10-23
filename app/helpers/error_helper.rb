159253a9ee63dbd466d1d7f2c59cbc7be9169c88
Cate Uselton <cathode.use@gmail.com>
2014-10-23 10:04:39 -0700

    error-helper complete

--- app/helpers/error_helper.rb
+++ app/helpers/error_helper.rb
@@ -1,17 +1,14 @@
 module ErrorHelper
 
   def error_message(obj)
-    html =
-    "<% if .errors.any? %>
-      <ul class='error-messages'>
-        <% #{obj}.errors.each do |column_name, message| %>
-          <li>
-            <strong><%= column_name.capitalize %></strong> <%= message %>
-          </li>
-        <% end %>
-      </ul>
-     <% end %>"
-     html.html_safe
+    if obj.errors.any?
+      html =  "<ul class='error_messages'>"
+      obj.errors.each do |column_name, message|
+        html += "<li> <strong>" + column_name.to_s.capitalize + "</strong> " + message + "</li>"
+      end
+      html += "</ul>"
+      html.html_safe
+    end
   end
 
 

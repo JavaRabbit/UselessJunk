c40890cbda24a70c937711a0b4a6f8376f8273eb
Cate Uselton <cathode.use@gmail.com>
2014-10-23 16:28:57 -0700

    pictures crop/resize on homepage

--- app/helpers/rating_helper.rb
+++ app/helpers/rating_helper.rb
@@ -1,9 +1,13 @@
 module RatingHelper
   def average_rating(product)
-    average = 0
-    product.reviews.each do |review|
-      average += review.rating.to_i
+    if product.reviews.empty?
+      return "No rating data"
+    else
+      average = 0
+      product.reviews.each do |review|
+        average += review.rating.to_i
+      end
+        average / product.reviews.count
     end
-      average/product.reviews.count
   end
 end

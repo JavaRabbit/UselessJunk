132787aa34dd71b56be66d70c1fe26d714fb22e8
Cate Uselton <cathode.use@gmail.com>
2014-10-23 22:04:09 -0700

    category sort updated to work with retired products

--- app/helpers/retired_helper.rb
+++ app/helpers/retired_helper.rb
@@ -1,11 +1,5 @@
 module RetiredHelper
 
-  def retired_products
-    retired_products = []
-    Product.all.each do |product|
-      if product.retired then retired_products << product end
-    end
-    retired_products
-  end
-  
+
+
 end
